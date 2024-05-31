3674d49bfc7a8071303da451ffc89580bad61b0c
Ron Boger <ronboger@gmail.com>
2024-05-31 14:04:43 -0700

    new code for SVA results that includes the shuffling of data with each point

--- pfam/sva_results.py
+++ pfam/sva_results.py
@@ -6,10 +6,7 @@ from tqdm import tqdm
 from protein_conformal.util import *
 import datetime
 
-def main(args):
-    data = np.load(args.input, allow_pickle=True)
-    n_calib = args.n_calib  # Number of calibration data points
-
+def run_trial(data, n_calib, args):
     np.random.shuffle(data)
     cal_data = data[:n_calib]
     test_data = data[n_calib:3*n_calib]
@@ -21,18 +18,22 @@ def main(args):
     X_test = X_test.flatten()
     y_test_exact = y_test_exact.flatten()
 
-    sva_results = []
     # generate random indices in the test set
-    percent_sva_test = args.percent_sva_test / 100
-    print(len(X_test) * args.percent_sva_test)
-    i_s = np.random.randint(0, len(X_test), int(len(X_test) * args.percent_sva_test))
+    i = np.random.randint(0, len(X_test))
+    # i_s = np.random.randint(0, len(X_test), int(len(X_test) * args.percent_sva_test))
 
-    print(f'Running SVA on {len(i_s)} samples with {len(X_test)} test samples.')
-    for _, i in tqdm(enumerate(i_s)):
-        p_0, p_1 = simplifed_venn_abers_prediction(X_cal, y_cal, X_test[i])
-        # np.mean(p_0, p_1)
-        sva_results.append((np.mean([p_0, p_1]), X_test[i], y_test_exact[i]))
-        # print(f'Prediction: {p_1}, Actual: {y_test[i]}')
+    p_0, p_1 = simplifed_venn_abers_prediction(X_cal, y_cal, X_test[i])
+    result = (np.mean([p_0, p_1]), X_test[i], y_test_exact[i])
+    return result
+
+def main(args):
+    data = np.load(args.input, allow_pickle=True)
+    n_calib = args.n_calib  # Number of calibration data points
+
+    sva_results = []
+    for trial in tqdm(range(args.num_trials)):
+        # print(f'Running trial {i+1} of {args.num_trials}')
+        sva_results.append(run_trial(data, n_calib, args))
     
     df_sva = pd.DataFrame(sva_results, columns=['p', 'x', 'y'])
     output_file = args.output + ('_' + str(datetime.datetime.now().date()) if args.add_date else '') + '.csv'
@@ -47,9 +48,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='/data/ron/protein-conformal/data/conformal_pfam_with_lookup_dataset.npy', help='Input file for the data')
     # '/data/ron/protein-conformal/data/conformal_pfam_with_lookup_dataset.npy'
-    parser.add_argument('--percent_sva_test', type=float, default=.1, help='percent of data to use for SVA testing')
+    # parser.add_argument('--percent_sva_test', type=float, default=.1, help='percent of data to use for SVA testing')
     # parser.add_argument('--alpha', type=float, default=0.1, help='Alpha value for the algorithm')
-    # parser.add_argument('--num_trials', type=int, default=100, help='Number of trials to run')
+    parser.add_argument('--num_trials', type=int, default=100, help='Number of trials to run')
     parser.add_argument('--n_calib', type=int, default=50, help='Number of calibration data points')
     # parser.add_argument('--delta', type=float, default=0.5, help='Delta value for the algorithm')
     parser.add_argument('--output', type=str, default='/data/ron/protein-conformal/data/sva_results', help='Output file for the results')
