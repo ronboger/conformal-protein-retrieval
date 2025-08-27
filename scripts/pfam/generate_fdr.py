30c30225c9b4fe1a253fb35872342b58980c951d
Ron Boger <ronboger@gmail.com>
2025-08-27 22:40:23 +0000

    bunch of renaming and refactoring

--- scripts/pfam/generate_fdr.py
+++ scripts/pfam/generate_fdr.py
@@ -1,73 +1,66 @@
+import datetime
 import numpy as np
 import pandas as pd
 import argparse
-import datetime
 import os
 from tqdm import tqdm
 from protein_conformal.util import *
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate FNR thresholds for different alpha values')
+    parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--alpha', type=float, default=0.1, help='Alpha value for the algorithm')
     parser.add_argument('--partial', type=bool, default=False, help='Partial hits')
     parser.add_argument('--num_trials', type=int, default=100, help='Number of trials to run')
     parser.add_argument('--n_calib', type=int, default=1000, help='Number of calibration data points')
-    parser.add_argument('--output', type=str, default='/data/ron/protein-conformal/data/pfam_fnr.npy', help='Output file for the results')
+    parser.add_argument('--delta', type=float, default=0.5, help='Delta value for the algorithm')
+    parser.add_argument('--output', type=str, default='/data/ron/protein-conformal/data/pfam_fdr.npy', help='Output file for the results')
     parser.add_argument('--add_date', type=bool, default=True, help='Add date to output file name')
     parser.add_argument('--data_path', type=str, default=None, help='Path to the pfam data file')
     args = parser.parse_args()
     alpha = args.alpha
     num_trials = args.num_trials
     n_calib = args.n_calib
+    delta = args.delta
     partial = args.partial
     
-    # Determine data path - use relative path if not specified
     if args.data_path is None:
-        # Get the directory where this script is located
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        project_root = os.path.dirname(script_dir)
+        project_root = os.path.dirname(os.path.dirname(script_dir))
         data_path = os.path.join(project_root, 'data', 'conformal_pfam_with_lookup_dataset.npy')
     else:
         data_path = args.data_path
     
     print(f"Loading data from: {data_path}")
-    
-    # Load the data
     data = np.load(data_path, allow_pickle=True)
 
-    fnrs = []
-    lhats = []
+    risks = []
     tprs = []
-    fprs = []
-    
+    lhats = []
+    fdr_cals = []
     for trial in tqdm(range(num_trials)):
         np.random.shuffle(data)
         cal_data = data[:n_calib]
         test_data = data[n_calib:]
         X_cal, y_cal = get_sims_labels(cal_data, partial=partial)
         X_test, y_test_exact = get_sims_labels(test_data, partial=partial)
-        _, y_test_partial = get_sims_labels(test_data, partial=True)
-        
-        lhat = get_thresh_new(X_cal, y_cal, alpha)
+        lhat, fdr_cal = get_thresh_FDR(y_cal, X_cal, alpha, delta, N=100)
         lhats.append(lhat)
-        
-        error, fraction_inexact, error_partial, fraction_partial, fpr = validate_lhat_new(X_test, y_test_partial, y_test_exact, lhat)
-        fnrs.append(error)
-        fprs.append(fpr)
+        fdr_cals.append(fdr_cal)
+        risks.append(risk(X_test, y_test_exact, lhat))
         tprs.append(calculate_true_positives(X_test, y_test_exact, lhat))
     
-    print("FNR: ", np.mean(fnrs))
+    print("Risk: ", np.mean(risks))
     print("TPR: ", np.mean(tprs))
     print("Lhat: ", np.mean(lhats))
-    print("FPR: ", np.mean(fprs))
+    print("FDR Cal: ", np.mean(fdr_cals))
 
     output_file = args.output + ('_' + str(datetime.datetime.now().date()) if args.add_date else '' + '.npy')
 
     np.save(output_file, 
-            {'fnrs': fnrs,
+            {'risks': risks,
              'tprs': tprs, 
              'lhats': lhats,
-             'fprs': fprs})
-    
+             'fdr_cals': fdr_cals})
+
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
+
 setup(
-    name = 'protein_conformal',
-    packages = find_packages(),
+    name='protein_conformal',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
 )
